31f26602211f2c96559fc84182b4309ff2125ed1
Fenil Kapopara <fenilkapopara34@gmail.com>
2025-12-31 13:28:30 +0530

    learn bst basic

---
+++ Foundation/BST/insert.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        
+class BST:
+    def __init__(self,root=None):
+        self.root=root
+    
+    def insert(self,root,val):
+        if not root:
+            return Node(val)
+        
+        if val<root.data:
+            root.left=self.insert(root.left,val)
+        else:
+            root.right=self.insert(root.right,val)
+        
+        return root
+    def inorder(self,root):
+        if not root:
+            return
+        self.inorder(root.left)
+        print(root.data, end=" ")
+        self.inorder(root.right)
+
+
+values = [10, 5, 20, 3, 7, 15]
+
+bst = BST()
+root = None
+for v in values:
+    root = bst.insert(root, v)
+bst.inorder(root)
